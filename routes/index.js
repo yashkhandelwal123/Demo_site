07e4fb2e25b5cc85f549da3feccc4906b425a11b
yashkhandelwal123 <yashnannu123@gmail.com>
2022-07-11 17:20:00 +0530

    routes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/' , homeController.home);
 
+router.use('/user' , require('./users'));
+
 
 
 
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+const userController = require('../controllers/user_controller');
+
+router.get('/profile' , userController.profile);
+
+module.exports = router;
