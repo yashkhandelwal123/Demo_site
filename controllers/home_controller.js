26a8e8e189a2840c26de6e99f598d0a4e667a5bb
yashkhandelwal123 <yashnannu123@gmail.com>
2022-07-27 23:49:57 +0530

    raat ka commit

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,13 +2,27 @@ const Post =  require('../models/post')
 
 module.exports.home = function(req ,res){
     // console.log(req.cookies);
-   Post.find({} , function(err , post){
-    return res.render('home.ejs' , {
-        title : "home",
-        post : post
-    });
-   });
+//    Post.find({} , function(err , post){
+//     return res.render('home.ejs' , {
+//         title : "home",
+//         post : post
+//     });
+//    });
     // return res.render('home.ejs' , {
     //     title : "home"
+    // });
+    Post.find({}).populate('user.users').exec(function(err, posts){
+        return res.render('home', {
+            title: "Home",
+            posts:  posts
+        });
+    })
+
+
+    // Post.find({}).lean().populate('user').exec(function(err,post){
+    //     return res.render('home.ejs' , {
+    //         title : "home",
+    //         post : post
+    //     });
     // });
 }
