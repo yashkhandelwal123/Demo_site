25e806506ffa8d165d537b6fef35c64afae62e32
yashkhandelwal123 <yashnannu123@gmail.com>
2022-08-01 16:59:01 +0530

    delete post

--- routes/post.js
+++ routes/post.js
@@ -7,5 +7,6 @@ const postController = require('../controllers/post_controller');
 
 router.post('/create' ,passport.checkAuthentication, postController.create);
 
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 
 module.exports = router; 
