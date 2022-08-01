e04061cd5e4aa9b2adbf75b25dc2013a93d0dcd2
yashkhandelwal123 <yashnannu123@gmail.com>
2022-08-01 17:46:41 +0530

    code distributed

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -19,3 +19,19 @@ module.exports.create = function(req,res){
         }
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(comment.user == req.user.id){
+            let postid = comment.post;
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postid , { $pull: {comments: req.params.id}} ,function(err,post){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
--- routes/comments.js
+++ routes/comments.js
@@ -7,5 +7,5 @@ const commentsController = require('../controllers/comments_controller');
 
 router.post('/create' ,passport.checkAuthentication, commentsController.create);
 
-
+router.get('/destroy/:id', passport.checkAuthentication , commentsController.destroy)
 module.exports = router; 
