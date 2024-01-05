0db922f2ca5372cf391d3e3cc183a3723712b27a
YashKhandelwal-tc <yash.khandelwal@techconfer.in>
2024-01-05 18:42:42 +0530

    changes done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,39 +3,41 @@ const User = require('../models/user');
 
 
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
+module.exports.home = async function(req, res){
 
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
+    try{
+         // populate the user of each post
+        let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+    
+        let users = await User.find({});
 
-    // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users: users
-            });
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: users
         });
 
-       
-    })
-
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+   
 }
 
 // module.exports.actionName = function(req, res){}
+
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then()
--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware =require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 
 app.use(sassMiddleware({
@@ -64,6 +67,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+app.use(flash());
+app.use(customMware.setFlash);
+
 
 // use express router
 app.use('/', require('./routes'));
